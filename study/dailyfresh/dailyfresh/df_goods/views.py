92513085f9869dca480391682c431face381d9e2
wys <251148036@qq.com>
2017-12-27 23:36:31 +0800

    新增了首页模块

--- study/dailyfresh/dailyfresh/df_goods/views.py
+++ study/dailyfresh/dailyfresh/df_goods/views.py
@@ -1,5 +1,33 @@
+#_*_coding:utf-8_*_
 from django.shortcuts import render
-
+from models import *
 def index(request):
-    context={'page_cart':1}
+    typelist=TypeInfo.objects.all()
+    #查询各分类取最新4条与最热4条
+    type0=typelist[0].goodsinfo_set.order_by('-id')[0:4]
+    type01 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
+    type1 = typelist[1].goodsinfo_set.order_by('-id')[0:4]
+    type11 = typelist[1].goodsinfo_set.order_by('-gclick')[0:4]
+    type2 = typelist[2].goodsinfo_set.order_by('-id')[0:4]
+    type21 = typelist[2].goodsinfo_set.order_by('-gclick')[0:4]
+    type3 = typelist[3].goodsinfo_set.order_by('-id')[0:4]
+    type31 = typelist[3].goodsinfo_set.order_by('-gclick')[0:4]
+    type4 = typelist[4].goodsinfo_set.order_by('-id')[0:4]
+    type41 = typelist[4].goodsinfo_set.order_by('-gclick')[0:4]
+    type5 = typelist[5].goodsinfo_set.order_by('-id')[0:4]
+    type51 = typelist[5].goodsinfo_set.order_by('-gclick')[0:4]
+    for g in type0:
+        print g.id
+        print g.gtitle
+        print g.gpric
+        print g.gpic
+    context={'title':'首页','page_cart':1,
+             'type0':type0,'type01':type01,
+             'type1': type1, 'type11': type11,
+             'type2': type2, 'type21': type21,
+             'type3': type3, 'type31': type31,
+             'type4': type4, 'type41': type41,
+             'type5': type5, 'type51': type51,
+             }
+    print 'typ01'
     return  render(request,'df_goods/index.html',context)
