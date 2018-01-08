f75fee5f0a7a5e67fd802db1c5034e2ccf297fef
wys <251148036@qq.com>
2018-01-08 22:36:24 +0800

    新增检索功能，未完成

--- study/dailyfresh/dailyfresh/dailyfresh/settings.py
+++ study/dailyfresh/dailyfresh/dailyfresh/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'df_cart',
     'df_order',
     'tinymce',
+    'haystack',#全文检索的应用
 )
 
 MIDDLEWARE_CLASSES = (
@@ -124,3 +125,19 @@ TINYMCE_DEFAULT_CONFIG = {
     'width': 600,
     'height': 400,
 }
+
+#搜索引擎
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        #使用Whoosh引擎
+        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        #索引文件路径
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
+    }
+}
+
+#当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+HAYSTACK_DEFAULT_OPERATOR='OR'
+#每页显示多少个数
+HAYSTACK_SEARCH_RESULTS_PER_PAGE=18
